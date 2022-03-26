0b9ebd920d690b791f5903f4891d4d6670607242
Eric Hafvenstein <81645330+theFlexOne@users.noreply.github.com>
2022-03-26 01:18:03 -0500

    deleted albums_controller due to weird error

--- app/controllers/albums_controller.rb
+++
@@ -1,8 +0,0 @@
-require_relative "./application_controller.rb"
-
-class AlbumsController < ApplicationController
-  get "/albums" do
-    albums = Album.all
-    albums.to_json(include: :tracks)
-  end
-end

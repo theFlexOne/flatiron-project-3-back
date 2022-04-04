7e3714e08b919979f4a7d42e20311688bd4412a6
Eric Hafvenstein <81645330+theFlexOne@users.noreply.github.com>
2022-04-04 11:04:13 -0500

    helper methods file controller_helpers.rb created

---
+++ app/helpers/controller_helpers.rb
@@ -0,0 +1,48 @@
+module ControllerHelpers
+  def build_tracks(tracks)
+    tracks.map do |track|
+      {
+        id: track.id,
+        name: track.name,
+        artist: {
+          id: track.artist&.id || nil,
+          name: track.artist&.name || "",
+          img_url: track.artist&.img_url || "",
+        },
+        album: {
+          id: track.album&.id || nil,
+          name: track.album&.name || "",
+          img_url: track.album&.img_url || "",
+        },
+        duration: track.duration || nil,
+      }
+    end
+  end
+
+  def build_albums(albums)
+    albums.map do |album|
+      {
+        id: album.id,
+        name: album.name,
+        tracks: build_tracks(album.tracks),
+      }
+    end
+  end
+
+  def seconds_to_time_display(seconds)
+    hh = (seconds / 3600).to_s
+    mm = ((seconds % 3600) / 60).to_s
+    ss = (seconds % 60).to_s
+
+    segments = [hh, mm, ss].filter { |t| t != "0" }
+    formatted_segments = segments.map do |seg|
+      if seg.size == 2
+        seg
+      else
+        "0#{seg}"
+      end
+    end
+
+    formatted_segments.join(":")
+  end
+end

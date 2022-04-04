a14e27562f17fccd215e25dac1bbc084d2ffbd59
Eric Hafvenstein <81645330+theFlexOne@users.noreply.github.com>
2022-04-04 11:05:35 -0500

    added abums routes to application_controller.rb

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -18,4 +18,36 @@ class ApplicationController < Sinatra::Base
     puts req
     "TEST POST RECIEVED!!!"
   end
+
+  get "/albums" do
+    albums = Album.all
+    # data = albums.map do |album|
+    #   {
+    #     id: album.id,
+    #     name: album.name,
+    #     artist: album.artist.name,
+    #     img_url: album.img_url,
+    #     release_date: album.release_date,
+    #     tracks: album.tracks,
+    #   }
+    # end
+    # data.to_json()
+    albums.to_json
+  end
+
+  get "/albums/:id" do |id|
+    # binding.pry
+    album = Album.find_by(id: id)
+    tracks = build_tracks(album.tracks)
+    data = {
+      id: album.id,
+      name: album.name,
+      artist: {
+        id: album.artist.id,
+        name: album.artist.name,
+        img_url: album.artist.img_url,
+      },
+    }
+    data.to_json
+  end
 end

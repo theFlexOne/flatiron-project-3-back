ab4e56c6a338393d8a36030a2f7b066a8b6f9bc1
Eric Hafvenstein <81645330+theFlexOne@users.noreply.github.com>
2022-04-04 11:07:22 -0500

    added methods to playlist_controller.rb for querying

--- app/models/playlist.rb
+++ app/models/playlist.rb
@@ -6,8 +6,36 @@ class Playlist < ActiveRecord::Base
   has_many :genres, through: :artists
   belongs_to :user
 
-  # def self.by_user(id)
-  #   RSpotify::User.find(id)
-  # end
+  def self.create_and_add_track(playlist, attr)
+    playlist = (playlist.class == Integer) ? Playlist.find(playlist) : playlist
+    playlist.tracks.create(attr)
+  end
 
+  def self.remove_track(playlist, track)
+    playlist = (playlist.class == Integer) ? Playlist.find(playlist) : playlist
+    playlist.tracks.delete(track)
+  end
+
+  def self.add_track_id(playlist, track_id)
+    playlist.track_ids = [*playlist.track_ids, track_id]
+  end
+
+  def add_track_id(track_id)
+    Playlist.add_track_id(self, track_id)
+  end
+
+  def add_track_by(attr)
+    track = Track.find_by(attr)
+    Playlist.add_track(self, track)
+  end
+
+  def remove_track(t)
+    return Error unless t.class == Track || t.class == Integer
+    self.tracks.delete(t)
+  end
+
+  def remove_duplicate_tracks
+    tracks = self.tracks.uniq
+    self.tracks = tracks
+  end
 end

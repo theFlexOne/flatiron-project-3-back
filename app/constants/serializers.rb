d04d016c5fe5289766cccb95a167fd3379cab93f
Eric Hafvenstein <81645330+theFlexOne@users.noreply.github.com>
2022-04-12 20:31:12 -0500

    seperated serializers into a file

--- app/constants/serializers.rb
+++ app/constants/serializers.rb
@@ -5,21 +5,53 @@ module Serializers
 
   ARTIST_SERIALIZER = {
     only: [:id, :name, :img_url],
+    include: {
+      albums: {
+        only: [:id, :name, :img_url, :release_date],
+      },
+    },
   }
 
   ALBUM_SERIALIZER = {
-    only: [:id, :name, :img_url],
-    include: { artist: ARTIST_SERIALIZER },
+    only: [:id, :name, :img_url, :release_date],
+    include: {
+      tracks: {
+        only: [:id, :name, :duration_s],
+        methods: [:duration],
+      },
+      artist: {
+        only: [:id, :name, :img_url],
+      },
+    },
   }
 
   TRACK_SERIALIZER = {
     only: [:id, :name, :duration_s],
     methods: [:duration],
-    include: { album: ALBUM_SERIALIZER },
+    include: {
+      album: {
+        only: [:id, :name, :img_url, :release_date],
+      },
+      artist: {
+        only: [:id, :name],
+      },
+      playlists: {
+        only: [:id, :name, :img_url],
+      },
+    },
   }
 
   PLAYLIST_SERIALIZER = {
     only: [:id, :name, :img_url],
-    include: { tracks: TRACK_SERIALIZER },
+    include: {
+      tracks: {
+        only: [:id, :name, :duration_s],
+        methods: [:duration],
+        include: {
+          album: { only: [:id, :name, :img_url], methods: [:album_release_date] },
+          artist: { only: [:id, :name, :img_url] },
+        },
+      },
+    },
   }
 end

e458f9c6ad0e91a3f5dd0262fe3ccb8196a9454c
Eric Hafvenstein <81645330+theFlexOne@users.noreply.github.com>
2022-04-12 20:29:56 -0500

    reseeded/fixed seeding issues

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,96 +5,27 @@ puts "Seeding..."
 # Import hard-coded Spotify artist id's
 spotify_ids = YAML.load_file("spotify_ids.yaml")
 
-artist_spotify_ids = spotify_ids[:artists]
-playlist_spotify_ids = spotify_ids[:playlists]
+spotify_playlists_ids = spotify_ids[:playlists]
 
-# Seeding music from Spotify with a list of Spotify artist ids
-artist_spotify_ids.each do |id|
-  s_artist = RSpotify::Artist.find(id)
-  s_albums = s_artist.albums
-
-  # Create artist
-  artist = Artist.find_or_create_by(spotify_id: s_artist.id)
-  artist_attributes = {
-    name: s_artist.name,
-    popularity: s_artist.popularity,
-    img_url: s_artist.images.last["url"],
-  }
-  artist.attributes = artist.attributes.merge artist_attributes
-  artist.save
-
-  # Create all albums for artist
-  s_albums.each do |s_album|
-    album = Album.find_or_create_by(spotify_id: s_album.id)
-    album_attributes = {
-      artist_id: artist.id,
-      name: s_album.name,
-      img_url: s_album.images.last["url"],
-      release_date: s_album.release_date,
-    }
-    album.attributes = album.attributes.merge album_attributes
-    album.save
-
-    # Create all tracks for each album by the artist
-    tracks = s_album.tracks
-    tracks.each do |s_track|
-      track = Track.find_or_create_by(spotify_id: s_track.id)
-      track_attributes = {
-        album_id: album.id,
-        name: s_track.name,
-        duration_s: (s_track.duration_ms / 1000),
-      }
-      track.attributes = track.attributes.merge track_attributes
-      track.save
-    end
-  end
-
-  total_albums = artist.albums.size
-  total_tracks = artist.albums.map { |a| a.tracks.size }.flatten.sum
-
-  puts "Created artist '#{artist.name}' with #{total_albums} albums & #{total_tracks} total tracks"
-end
-
-# Seeding my playlists from Spotify with a list of my Spotify playlist ids
-playlist_spotify_ids.each do |id|
-
-  # find playlist in spotify
+spotify_playlists_ids.each do |id|
   s_playlist = RSpotify::Playlist.find(ENV["MY_SPOTIFY_USER_ID"], id)
+  s_track_ids = s_playlist.tracks.map(&:id)
 
-  # find or create playlist in db
-  playlist = Playlist.find_or_create_by(spotify_id: s_playlist.id)
+  img_url = !s_playlist.images.empty? ? s_playlist.images.first["url"] : null
+  db_playlist = Playlist.find_or_create_by(name: s_playlist.name, spotify_id: s_playlist.id, img_url: img_url)
 
-  # fill in playlist attributes with spotify data
-  img_url = s_playlist.images.first["url"] unless s_playlist.images.empty?
-  playlist_attributes = {
-    name: s_playlist.name,
-    img_url: img_url,
-  }
-  playlist.attributes = playlist.attributes.merge playlist_attributes
+  s_track_ids.each do |id|
+    s_track = RSpotify::Track.find(id)
+    s_album = s_track.album
+    s_artist = s_track.artists.first
 
-  # get all tracks for playlist from spotify
-  s_playlist.tracks.each do |s_track|
+    db_artist = Artist.find_or_create_by(name: s_artist.name, spotify_id: s_artist.id, img_url: s_artist&.images&.first["url"] || "", popularity: s_artist.popularity)
+    db_album = Album.find_or_create_by(name: s_album.name, spotify_id: s_album.id, img_url: s_album&.images&.first["url"] || "", release_date: s_album.release_date, artist_id: db_artist.id)
+    db_track = Track.find_or_create_by(name: s_track.name, spotify_id: s_track.id, duration_s: s_track.duration_ms / 1000, album_id: db_album.id)
 
-    # find or create track in db
-    track = Track.find_or_create_by(spotify_id: s_track.id)
-
-    # fill in track attributes with spotify data
-    track_attributes = {
-      name: s_track.name,
-      duration_s: (s_track.duration_ms / 1000),
-    }
-    track.attributes = track.attributes.merge track_attributes
-
-    # save changes to track
-    track.save
-
-    # save track to playlist
-    PlaylistTrack.find_or_create_by(track_id: track.id, playlist_id: s_playlist.id)
+    PlaylistTrack.create(playlist_id: db_playlist.id, track_id: db_track.id)
+    p "created #{db_playlist.name} with #{db_playlist.albums.size} albums & #{db_playlist.tracks.size} tracks"
   end
-
-  # save changes to playlist
-  playlist.save
-  puts "Created playlist #{playlist.name} with #{playlist.tracks.size} tracks"
 end
 
 puts "Seeding done!"
